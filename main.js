aa8f640a5fb8362c06174de85441d743a11bdcc7
avamalinowski <ava.malinowski@gmail.com>
2025-06-26 17:38:01 -0700

    finishing assignment one

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import readline from "readline";
 
 const apiKey = process.env.GOOGLE_API_KEY;
 
@@ -15,5 +17,47 @@ const messages = [
     new HumanMessage("hi!"),
 ];
 
-const response = await model.invoke(messages);
+var response = await model.invoke(messages);
 console.log(response.content);
+//3 below all the same
+//await model.invoke("Hello");
+
+//await model.invoke([{ role: "user", content: "Hello" }]);
+
+//await model.invoke([new HumanMessage("hi!")]);
+
+const stream = await model.stream(messages);
+
+const chunks = [];
+for await (const chunk of stream) {
+    chunks.push(chunk);
+    console.log(`${chunk.content}|`);
+}
+
+
+const systemTemplate = "Translate the folowing from English into {language} and include the romaji translation";
+
+const promptTemplate = ChatPromptTemplate.fromMessages([
+    ["system", systemTemplate],
+    ["user", "{text}"],
+]);
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+r1.question("What would you like to translate to Japanese? ", async (userInput) => {
+    console.log("User input:", userInput);
+
+    const promptValue = await promptTemplate.invoke({
+        language: "japanese",
+        text: userInput,
+    });
+
+    response = await model.invoke(promptValue);
+    console.log(`${response.content}`);
+
+    r1.close();
+});
+
